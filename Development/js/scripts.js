f2bcefd0c4fbd070e6ac432fbf075f08261750d3
434-عمر بشار المسيليم <omaralmusaileem@gmail.com>
2020-06-27 21:01:34 +0300

    تم

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,40 @@
 // السؤال الأول
 // 1
-
+const name = ("omar");
 // 2
-
+Number =(15);
 // 3 and 4
-
+console.log("انا" + " "+ name + " "+ "و عمري " + " "+ Number);
 // 5 and 6
-
+console.log(Number*=5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let hobbys = ["football","basketball"];
+    console.log(hobbys);
 
 // 2
 
+hobbys.push(Number);
+console.log(hobbys);
 // 3
-
+hobbys.pop()
+console.log(hobbys);
 // 4
-
+let omar ={
+    track:"web", 
+    lang: "css",
+   ta : [
+       "hussain","nancy","rahf","modhi"
+   ]
+};
 // 5
 
 // 6
-
+console.log(omar.track);
 // 7
-
+console.log(omar.ta[0]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
